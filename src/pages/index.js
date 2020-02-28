f0568e8ce58b6d015c0dddb6d1e11a23048ef080
Piervincenzo Madeo <pier.made@gmail.com>
2020-02-28 16:36:14 +0100

    Run animation only at the first visit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Home from "../templates/Home";
 import HeroBanner from "../components/HeroBanner";
 import BottomProjectBanner from "../components/BottomProjectBanner";
 
+function useEffectOnce(cb) {
+  const didRun = useRef(false);
+
+  useEffect(() => {
+    if (!didRun.current) {
+      cb();
+      didRun.current = true;
+    }
+  });
+}
+
 export default () => {
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+
+  useEffectOnce(() => {
+    const isFirstVisit = localStorage.getItem("isFirstVisit");
+    if (isFirstVisit === null) {
+      localStorage.setItem("isFirstVisit", true);
+      setShouldAnimate(true);
+    } else {
+      localStorage.setItem("isFirstVisit", false);
+      setShouldAnimate(false);
+    }
+  });
+
   return (
-    <Home>
+    <Home animate={shouldAnimate}>
       <HeroBanner className="mt-48" />
       <BottomProjectBanner />
     </Home>
